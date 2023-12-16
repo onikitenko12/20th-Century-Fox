9fe1baa9c60f5bf15157b199a58b2aea937a10e6
Val <yermakvaleriy@gmail.com>
2023-12-16 23:23:53 +0200

    Changed classAddressBook > classContactBook

--- src/classContactBook.py
+++ src/classContactBook.py
@@ -1,5 +1,34 @@
 from collections import UserDict
+from classRecord import Record
+
 
 class ContactBook(UserDict):
-    #A class representing an address book that stores records.
-    NotImplemented
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        for record_name, record in self.data.items():
+            if record_name == name:
+                return record
+        return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+            print(f"The contact {name} deleted.")
+        else:
+            print(f"The contact {name} not found.")
+
+    def add(self, name, address):
+        self.data[name] = address
+
+    def iterator(self, item_number):
+        counter = 0
+        result = ''
+        for item, record in self.data.items():
+            result += f'{item}: {record}\n'
+            counter += 1
+            if counter >= item_number:
+                yield result
+                counter = 0
+                result = ''
