1dd7aa04835289df4e1c39c6fcad042b749de4b6
onikitenko12 <oleksiy.nikitenko1@gmail.com>
2023-12-18 14:04:42 +0200

    Ticket6 - Added Note field iteraction Ticket7 - Added Tag functionality

--- src/classRecord.py
+++ src/classRecord.py
@@ -4,12 +4,14 @@ from classBirthday import Birthday
 from classMail import Email
 from classPhone import Phone
 from classContact import Name
+from classContactNotes import ContactNote
 
 
 class Record(Field):
     def __init__(self, name, birthday=None, email=None):
         self.name = Name(name)
         self.phones = []
+        self.notes = []
         self.birthday = Birthday(birthday) if birthday else None
         self.email = Email(email) if email else None
 
@@ -18,6 +20,51 @@ class Record(Field):
                 phones: {'; '.join(p.value for p in self.phones)}" \
                f", birthday: {self.birthday.value}, {self.email}" \
                 if self.birthday else ""
+    
+    def add_note(self, note_text: str):
+        note = ContactNote(note_text)
+        note.is_valid_note(note_text)
+        if note not in self.notes:
+            self.notes.append(note)
+    
+    def remove_note(self, note_to_remove):
+        is_found_note = False
+        for item in self.notes:
+            if item.value == note_to_remove:
+                self.notes.remove(item)
+                is_found_note = True
+                break
+        if not is_found_note:
+            raise ValueError('Note not found')
+        
+    def find_note(self, note_text:str):
+        is_found_note = False
+        for note in self.notes:
+            if note.value.find(note_text) != -1:
+                is_found_note = True
+                return note
+        if not is_found_note:
+            raise ValueError('Note not founded')
+
+    def get_all_notes(self):
+        result = [n.value for n in self.notes]
+        return result
+    
+    def add_tag(self, tag, note_text):
+        is_found_note = False
+        for index, item in enumerate(self.notes):
+            if item.value == note_text:
+                self.notes[index] = ContactNote(f"<{tag}>{note_text}")
+                is_found_note = True
+        if not is_found_note:
+            raise ValueError('Note not founded')
+        
+    def remove_tag(self, tag_to_remove):
+        for item in self.notes:
+            if item.value.find(tag_to_remove) != -1:
+                item.value = item.value.removeprefix(f"<{tag_to_remove}>")
+                return 
+        raise ValueError(f'Tag {tag_to_remove} not founded')
 
     def add_phone(self, phone_number: str):
         phone = Phone(phone_number)
