d28bcbe118d671d6480db0b1a72ed4d674b076d7
Val <yermakvaleriy@gmail.com>
2023-12-18 23:35:59 +0200

    Create writing posibility

--- src/classRecord.py
+++ src/classRecord.py
@@ -4,6 +4,7 @@ from classBirthday import Birthday
 from classMail import Email
 from classPhone import Phone
 from classContact import Name
+from collections import UserDict
 
 
 # entry in a contact
@@ -67,3 +68,74 @@ phones: {'; '.join(p.value for p in self.phones)}" \
         if self.birthday:
             return self.birthday.days_until_next_birthday()
         return None
+
+
+class ContactBook(UserDict):
+
+    # add and save new record of contact into the book
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+# find record of contact in book
+    def find(self, name):
+        return self.data.get(name)
+
+# delete record of contact from the book
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+            print(f"The contact {name} deleted.")
+        else:
+            print(f"The contact {name} not found.")
+
+# add new record of contact into the book
+    def add(self, name, address):
+        self.data[name] = address
+
+# forms a result string that contains information about each record as a string
+    def iterator(self, item_number):
+        counter = 0
+        result = ''
+        for item, record in self.data.items():
+            result += f'{item}: {record}\n'
+            counter += 1
+            if counter >= item_number:
+                yield result
+                counter = 0
+                result = ''
+
+    def edit(self, name, new_record: Record):
+        if name in self.data:
+            self.data[name] = new_record
+            print(f"The contact {name} has been edited.")
+        else:
+            print(f"The contact {name} not found.")
+
+
+address_book = ContactBook()
+
+# # Створення контакту
+# record1 = Record(name="John Doe", birthday="01.01.1990",
+#                  email="john@example.com")
+# record2 = Record(name="Jane Doe", birthday="05.05.1995",
+#                  email="jane@example.com")
+# print(record1)
+# print(record2)
+# # Додавання контактів до адресної книги
+# address_book.add_record(record1)
+# address_book.add_record(record2)
+
+# # Виведення всіх контактів
+# print("All contacts:")
+# for item, record in address_book.data.items():
+#     print(f'{item}: {record}')
+
+# # Редагування контакту
+# edited_record = Record(name="John Doe", birthday="01.01.1990",
+#                        email="john.doe@example.com")
+# address_book.edit("John Doe", edited_record)
+
+# # Виведення всіх контактів після редагування
+# print("\nAll contacts after editing:")
+# for item, record in address_book.data.items():
+#     print(f'{item}: {record}')
